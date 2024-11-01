147f179da8724b052531eff6b68e89facf28a0a3
szeyu <szeyusim@gmail.com>
2024-11-01 23:12:44 +0800

    [Ground] Add Ground class to handle scrolling ground in Dino Game

---
+++ js/ground.js
@@ -0,0 +1,26 @@
+// js/ground.js
+export default class Ground {
+    constructor(ctx) {
+        this.ctx = ctx;
+        this.image = new Image();
+        this.image.src = 'assets/ground.png';
+        this.x = 0;
+        this.y = 300;
+        this.width = 800;
+        this.speed = 5;
+    }
+
+    render() {
+        // Draw the ground image twice to create a continuous scrolling effect
+        this.ctx.drawImage(this.image, this.x, this.y, this.width, this.image.height);
+        this.ctx.drawImage(this.image, this.x + this.width, this.y, this.width, this.image.height);
+
+        // Move the ground to the left for scrolling effect
+        this.x -= this.speed;
+
+        // Reset position when the ground goes off screen
+        if (this.x <= -this.width) {
+            this.x = 0;
+        }
+    }
+}
